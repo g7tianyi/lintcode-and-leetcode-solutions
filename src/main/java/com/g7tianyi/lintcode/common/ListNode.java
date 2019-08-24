1af32d14067cdedf6bcb0fc5ad253759cf965d5d
g7tianyi <g7tianyi@gmail.com>
2019-08-24 15:47:34 +0800

    feat#lintcode#g7tianyi#partition-list

--- src/main/java/com/g7tianyi/lintcode/common/ListNode.java
+++ src/main/java/com/g7tianyi/lintcode/common/ListNode.java
@@ -21,17 +21,6 @@ public class ListNode {
     return String.valueOf(val);
   }
 
-  public static ListNode makeList(int len) {
-
-    ListNode listNode = new ListNode(1);
-    ListNode currNode = listNode;
-    for (int i = 2; i <= len; i++) {
-      currNode.next = new ListNode(i);
-      currNode = currNode.next;
-    }
-    return listNode;
-  }
-
   public static void print(ListNode listNode) {
     StringBuilder sb = new StringBuilder("[");
     while (listNode != null) {
@@ -43,5 +32,45 @@ public class ListNode {
     }
     sb.append("]");
     log.info(sb.toString());
+  }
+
+  public static ListNode makeList(int len) {
+
+    ListNode listNode = new ListNode(1);
+    ListNode currNode = listNode;
+    for (int i = 2; i <= len; i++) {
+      currNode.next = new ListNode(i);
+      currNode = currNode.next;
+    }
+    return listNode;
+  }
+
+  public static ListNode makeRandomList(int len) {
+
+    ListNode listNode = new ListNode(randomInt(len + 1));
+    ListNode currNode = listNode;
+    for (int i = 2; i <= len; i++) {
+      currNode.next = new ListNode(randomInt(len + 1));
+      currNode = currNode.next;
+    }
+    return listNode;
+  }
+
+  public static ListNode makeListFrom(int ...args) {
+    if (args.length == 0) {
+      return null;
+    }
+
+    ListNode listNode = new ListNode(0);
+    ListNode currNode = listNode;
+    for (int arg: args) {
+      currNode.next = new ListNode(arg);
+      currNode = currNode.next;
+    }
+    return listNode.next;
+  }
+
+  private static int randomInt(int max) {
+    return (int) (Math.random() * max);
   }
 }
---
+++ src/main/java/com/g7tianyi/lintcode/simple/PartitionList.java
@@ -0,0 +1,106 @@
+package com.g7tianyi.lintcode.simple;
+
+import com.g7tianyi.lintcode.common.ListNode;
+import com.g7tianyi.lintcode.util.Log;
+import lombok.AllArgsConstructor;
+import org.junit.Test;
+
+import java.util.function.Consumer;
+
+/**
+ * Created by g7tianyi on Aug 24, 2019
+ *
+ * <p>Problem link: https://www.lintcode.com/problem/partition-list/description
+ */
+public class PartitionList {
+
+  private static final Log log = new Log();
+
+  public class Solution {
+
+    public ListNode partition(ListNode head, int x) {
+
+      if (head == null || head.next == null) {
+        return head;
+      }
+
+      ListNode prev = null;
+      ListNode curr = head;
+      while (curr != null && curr.val < x) {
+        prev = curr;
+        curr = curr.next;
+      }
+      if (curr == null) { // (*)
+        return head;
+      }
+
+      // 输入: 1->4->3->2->5->2->null, x = 3
+      // 输出: 1->2->2->4->3->5->null
+
+      ListNode small = prev;
+
+      // 单链表，当我们把一个节点
+      // 1) 移动到头部时：需要修改节点的前向指针的next指针、自身的next指针和头指针，见代码(#1)行
+      // 2) 移动到另一节点P的后面：需要修改节点的前向指针的next、自身的next指针和P的next指针，见代码(#2)行
+      while (curr != null) {
+        ListNode next = curr.next;
+        if (curr.val < x) {
+          prev.next = curr.next; // 因为(*)行的检查，所以prev不可能会为null
+          if (small == null) { // (#1)
+            curr.next = head;
+            small = curr;
+            head = small;
+          } else { // (#2)
+            curr.next = small.next;
+            small.next = curr;
+            small = curr;
+          }
+        } else {
+          prev = curr;
+        }
+
+        curr = next;
+      }
+
+      return head;
+    }
+  }
+
+  @AllArgsConstructor
+  private static final class Argument {
+
+    private ListNode head;
+
+    private int x;
+  }
+
+  @Test
+  public void test() {
+
+    Solution s = new Solution();
+
+    Consumer<Argument> runner =
+        argument -> {
+          log.info("x: %d", argument.x);
+          ListNode.print(argument.head);
+          ListNode.print(s.partition(argument.head, argument.x));
+          log.info("\n");
+        };
+
+    runner.accept(new Argument(null, 0));
+    runner.accept(new Argument(ListNode.makeRandomList(1), 1));
+    runner.accept(new Argument(ListNode.makeRandomList(2), 10));
+    runner.accept(new Argument(ListNode.makeRandomList(2), -1));
+    runner.accept(new Argument(ListNode.makeRandomList(2), 1));
+    runner.accept(new Argument(ListNode.makeRandomList(16), 5));
+    runner.accept(new Argument(ListNode.makeRandomList(16), 0));
+    runner.accept(new Argument(ListNode.makeRandomList(15), 100000));
+    runner.accept(new Argument(ListNode.makeListFrom(1, 4, 3, 2, 5, 2), 3));
+    runner.accept(
+        new Argument(
+            ListNode.makeListFrom(15, 4, 9, 1, 11, 12, 15, 4, 2, 0, 15, 1, 14, 11, 16, 7), 5));
+    runner.accept(
+        new Argument(
+            ListNode.makeListFrom(8, 11, 13, 7, 11, 3, 6, 8, 3, 10, 12, 1, 14, 5, 11, 0), 5));
+  }
+}
