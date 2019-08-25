02ca9a9bc5aa3586690be75ea4a321a7d5388135
g7tianyi <g7tianyi@gmail.com>
2019-08-25 11:04:15 +0800

    feat#lintcode#g7tianyi#subarray-sum

---
+++ src/main/java/com/g7tianyi/lintcode/array/SubArraySum.java
@@ -0,0 +1,70 @@
+package com.g7tianyi.lintcode.array;
+
+import com.g7tianyi.lintcode.util.Console;
+import com.g7tianyi.lintcode.util.Log;
+import lombok.AllArgsConstructor;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+
+/**
+ * Created by g7tianyi on Aug 25, 2019
+ *
+ * <p>Problem link: https://www.lintcode.com/problem/subarray-sum/description
+ */
+public class SubArraySum {
+
+  private static final Log log = new Log();
+
+  public class Solution {
+    public List<Integer> subarraySum(int[] nums) {
+      // write your code here
+      int len = nums.length;
+
+      List<Integer> result = new ArrayList<>();
+
+      Map<Integer, Integer> map = new HashMap<>();
+
+      map.put(0, -1);
+
+      int sum = 0;
+      for (int i = 0; i < len; i++) {
+        sum += nums[i];
+        if (map.containsKey(sum)) {
+          result.add(map.get(sum) + 1);
+          result.add(i);
+          return result;
+        }
+        map.put(sum, i);
+      }
+
+      return result;
+    }
+  }
+
+  @AllArgsConstructor
+  public static class Input {
+
+    private int[] arr;
+  }
+
+  @Test
+  public void test() {
+
+    Solution s = new Solution();
+
+    Consumer<Input> runner =
+        input -> {
+          log.info(Console.stringify(input.arr));
+          log.info(Console.stringify(s.subarraySum(input.arr)));
+          log.info("");
+        };
+
+    runner.accept(new Input(new int[] {-3, 1, 2, -3, 4}));
+    runner.accept(new Input(new int[] {-3, 1, -4, 2, -3, 4}));
+  }
+}
