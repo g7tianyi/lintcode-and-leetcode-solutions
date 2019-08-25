2ba3a230cfaf111aca4d93ed98ed4e3c563aa6b7
g7tianyi <g7tianyi@gmail.com>
2019-08-25 16:26:56 +0800

    feat#lintcode#g7tianyi#left-pad

---
+++ src/main/java/com/g7tianyi/lintcode/string/LeftPad.java
@@ -0,0 +1,63 @@
+package com.g7tianyi.lintcode.string;
+
+import com.g7tianyi.lintcode.util.Log;
+import lombok.AllArgsConstructor;
+import org.junit.Test;
+
+import java.util.function.Consumer;
+
+/**
+ * Created by g7tianyi on Aug 25, 2019
+ *
+ * <p>Problem link: https://www.lintcode.com/problem/capitalizes-the-first-letter/description
+ */
+public class LeftPad {
+
+  private static final Log log = new Log();
+
+  public static class Solution {
+
+    public static String leftPad(String originalStr, int size) {
+      return leftPad(originalStr, size, ' ');
+    }
+
+    public static String leftPad(String originalStr, int size, char padChar) {
+      // write your code here
+
+      StringBuilder sb = new StringBuilder();
+      sb.append(originalStr);
+      sb.reverse();
+
+      int len = size - sb.length();
+      while (len > 0) {
+        sb.append(padChar);
+        len--;
+      }
+
+      sb.reverse();
+      return sb.toString();
+    }
+  }
+
+  @AllArgsConstructor
+  private static class Input {
+
+    private String s;
+
+    private int size;
+
+    private char padChar;
+  }
+
+  @Test
+  public void test() {
+
+    Consumer<Input> c =
+        input ->
+            log.info("[%s] => [%s]", input.s, Solution.leftPad(input.s, input.size, input.padChar));
+
+    c.accept(new Input("foo", 5, ' '));
+    c.accept(new Input("foobar", 6, ' '));
+    c.accept(new Input("1", 2, '0'));
+  }
+}
