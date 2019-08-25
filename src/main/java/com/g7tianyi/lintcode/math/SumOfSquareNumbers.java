efd8dd0660706e26bbb0b01a0f9d2e9ebb815094
g7tianyi <g7tianyi@gmail.com>
2019-08-25 12:57:36 +0800

    feat#lintcode#g7tianyi#sum-of-square-numbers

---
+++ src/main/java/com/g7tianyi/lintcode/math/SumOfSquareNumbers.java
@@ -0,0 +1,56 @@
+package com.g7tianyi.lintcode.math;
+
+import com.g7tianyi.lintcode.util.Log;
+import org.junit.Test;
+
+/**
+ * Created by g7tianyi on Aug 25, 2019
+ *
+ * <p>Problem link: https://www.lintcode.com/problem/sum-of-square-numbers/description
+ */
+public class SumOfSquareNumbers {
+
+  private static final Log log = new Log();
+
+  public class Solution {
+
+    public boolean checkSumOfSquareNumbers(int num) {
+      // write your code here
+      if (num < 0) {
+        return false;
+      } else if (num == 0) {
+        return true;
+      }
+
+      int upper = (int) Math.ceil(Math.sqrt((double) num));
+
+      int a, b;
+      for (int i = 0; i < upper; i++) {
+        a = i * i;
+        for (int j = i + 1; j <= upper; j++) {
+          b = a + j * j;
+          if (b == num) {
+            return true;
+          }
+          if (b > num) {
+            break;
+          }
+        }
+      }
+
+      return false;
+    }
+  }
+
+  @Test
+  public void test() {
+
+    Solution s = new Solution();
+
+    log.info(s.checkSumOfSquareNumbers(1));
+    log.info(s.checkSumOfSquareNumbers(5));
+    log.info(s.checkSumOfSquareNumbers(-5));
+    log.info(s.checkSumOfSquareNumbers(1546112212));
+    log.info(s.checkSumOfSquareNumbers(Integer.MAX_VALUE));
+  }
+}
