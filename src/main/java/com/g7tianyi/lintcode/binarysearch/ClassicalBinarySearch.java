df23bd9fdc41f546b0fb9a14340eef8a5322cd24
g7tianyi <g7tianyi@gmail.com>
2019-08-25 12:40:31 +0800

    feat#lintcode#g7tianyi#classical-binary-search

---
+++ src/main/java/com/g7tianyi/lintcode/binarysearch/ClassicalBinarySearch.java
@@ -0,0 +1,44 @@
+package com.g7tianyi.lintcode.binarysearch;
+
+import com.g7tianyi.lintcode.util.Log;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by g7tianyi on Aug 25, 2019
+ *
+ * <p>Problem link: https://www.lintcode.com/problem/classical-binary-search/description
+ */
+public class ClassicalBinarySearch {
+
+  private static final Log log = new Log();
+
+  public class Solution {
+
+    public int findPosition(int[] arr, int value) {
+      int former = 0, latter = arr.length - 1, middle;
+      while (former <= latter) {
+        middle = former + ((latter - former) >> 1);
+        if (arr[middle] == value) {
+          return middle;
+        }
+
+        if (arr[middle] < value) {
+          former = middle + 1;
+        } else {
+          latter = middle - 1;
+        }
+      }
+      return -1;
+    }
+  }
+
+  @Test
+  public void test() {
+
+    Solution s = new Solution();
+
+    Assert.assertEquals(2, s.findPosition(new int[] {1, 2, 2, 4, 5, 5}, 2));
+    Assert.assertEquals(-1, s.findPosition(new int[] {1, 2, 2, 4, 5, 5}, 6));
+  }
+}
