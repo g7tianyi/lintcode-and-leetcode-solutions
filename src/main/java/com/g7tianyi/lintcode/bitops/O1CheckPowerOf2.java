da34eda60b13c9fd99db1d7ca07ec9ee693a4ec6
g7tianyi <g7tianyi@gmail.com>
2019-08-25 17:22:45 +0800

    feat#lintcode#g7tianyi#o1-check-power-of-2

---
+++ src/main/java/com/g7tianyi/lintcode/bitops/O1CheckPowerOf2.java
@@ -0,0 +1,36 @@
+package com.g7tianyi.lintcode.bitops;
+
+import com.g7tianyi.lintcode.util.Log;
+import org.junit.Test;
+
+import java.util.function.Consumer;
+
+/**
+ * Created by g7tianyi on Aug 25, 2019
+ *
+ * <p>Problem link: https://www.lintcode.com/problem/o1-check-power-of-2/description
+ */
+public class O1CheckPowerOf2 {
+
+  private static final Log log = new Log();
+
+  public class Solution {
+
+    public boolean checkPowerOf2(int num) {
+      return num > 0 && (num & (num - 1)) == 0;
+    }
+  }
+
+  @Test
+  public void test() {
+
+    Solution s = new Solution();
+
+    Consumer<Integer> c = num -> log.info("%d => %s", num, s.checkPowerOf2(num));
+
+    c.accept(4);
+    c.accept(5);
+    c.accept(64);
+    c.accept(0);
+  }
+}
