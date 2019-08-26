5057b4f3b859b6b942650ceb33e5e010592c5726
g7tianyi <g7tianyi@gmail.com>
2019-08-26 20:34:17 +0800

    feat#lintcode#g7tianyi#minimum-depth-of-binary-tree

--- src/main/java/com/g7tianyi/lintcode/common/TreeNode.java
+++ src/main/java/com/g7tianyi/lintcode/common/TreeNode.java
@@ -1,5 +1,8 @@
 package com.g7tianyi.lintcode.common;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /** Created by g7tianyi on Aug 24, 2019 */
 public class TreeNode {
 
@@ -12,5 +15,46 @@ public class TreeNode {
     this.val = val;
     this.left = null;
     this.right = null;
+  }
+
+  public static TreeNode createTree(String s) {
+    if (s == null) {
+      return null;
+    }
+    s = s.trim();
+    if (s.length() == 0) {
+      return null;
+    }
+
+    String[] elems = s.split(",");
+    List<TreeNode> nodes = new ArrayList<>(elems.length);
+    for (int i = 0, value; i < elems.length; i++) {
+      if (elems[i].equals("#")) {
+        nodes.add(null);
+        continue;
+      }
+
+      value = Integer.valueOf(elems[i]);
+      TreeNode node = new TreeNode(value);
+      nodes.add(node);
+
+      if (i == 0) {
+        continue;
+      }
+
+      int parentIndex = ((i + 1) >> 1) - 1;
+      TreeNode parentNode = nodes.get(parentIndex);
+      if (parentNode == null) {
+        continue;
+      }
+
+      if (i % 2 == 1) {
+        parentNode.left = node;
+      } else {
+        parentNode.right = node;
+      }
+    }
+
+    return nodes.get(0);
   }
 }
---
+++ src/main/java/com/g7tianyi/lintcode/tree/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,61 @@
+package com.g7tianyi.lintcode.tree;
+
+import com.g7tianyi.lintcode.common.TreeNode;
+import com.g7tianyi.lintcode.util.Log;
+import lombok.AllArgsConstructor;
+import org.junit.Test;
+
+import java.util.function.Consumer;
+
+/**
+ * Created by g7tianyi on Aug 26, 2019
+ *
+ * <p>Problem link: https://www.lintcode.com/problem/minimum-depth-of-binary-tree/description
+ */
+public class MinimumDepthOfBinaryTree {
+
+  private static final Log log = new Log();
+
+  public class Solution {
+
+    public int minDepth(TreeNode root) {
+
+      if (root == null) {
+        return 0;
+      }
+
+      int left = minDepth(root.left);
+      int right = minDepth(root.right);
+
+      int subDepth;
+      if (left == 0 && right == 0) {
+        subDepth = 0;
+      } else if (left != 0 && right == 0) {
+        subDepth = left;
+      } else if (left == 0) {
+        subDepth = right;
+      } else {
+        subDepth = Math.min(left, right);
+      }
+
+      return subDepth + 1;
+    }
+  }
+
+  @AllArgsConstructor
+  private static final class Input {
+
+    private TreeNode root;
+  }
+
+  @Test
+  public void test() {
+
+    Solution s = new Solution();
+
+    Consumer<Input> runner = input -> log.info("%d", s.minDepth(input.root));
+
+    runner.accept(new Input(TreeNode.createTree("1,#,2,#,#,3")));
+    runner.accept(new Input(TreeNode.createTree("1,2,3,#,#,4,5")));
+  }
+}
