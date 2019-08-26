4f6d765774d49c8cf111361bd0d7a4ed0a0b408f
g7tianyi <g7tianyi@gmail.com>
2019-08-26 14:27:16 +0800

    feat#lintcode#g7tianyi#flip game

---
+++ src/main/java/com/g7tianyi/lintcode/string/FlipGame.java
@@ -0,0 +1,69 @@
+package com.g7tianyi.lintcode.string;
+
+import com.g7tianyi.lintcode.util.Console;
+import com.g7tianyi.lintcode.util.Log;
+import lombok.AllArgsConstructor;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * Created by g7tianyi on Aug 26, 2019
+ *
+ * <p>Problem link: https://www.lintcode.com/problem/flip-game/description
+ */
+public class FlipGame {
+
+  private static final Log log = new Log();
+
+  public class Solution {
+
+    public List<String> generatePossibleNextMoves(String s) {
+
+      List<String> result = new ArrayList<>();
+
+      if (s.length() < 2) {
+        return result;
+      }
+
+      char prev = s.charAt(0), curr;
+      for (int i = 1; i < s.length(); i++) {
+        curr = s.charAt(i);
+        if (prev == '+' && curr == '+') {
+          StringBuilder sb = new StringBuilder(s);
+          sb.setCharAt(i - 1, '-');
+          sb.setCharAt(i, '-');
+          result.add(sb.toString());
+        }
+        prev = curr;
+      }
+
+      return result;
+    }
+  }
+
+  @AllArgsConstructor
+  private static class Input {
+
+    private String s;
+  }
+
+  @Test
+  public void test() {
+
+    Solution s = new Solution();
+
+    Consumer<Input> c =
+        input -> {
+          String ret = Console.stringify(s.generatePossibleNextMoves(input.s));
+          log.info("[%s] => [%s]", input.s, ret);
+        };
+
+    c.accept(new Input("++"));
+    c.accept(new Input("--"));
+    c.accept(new Input("++++"));
+    c.accept(new Input("---+++-+++-+"));
+  }
+}
