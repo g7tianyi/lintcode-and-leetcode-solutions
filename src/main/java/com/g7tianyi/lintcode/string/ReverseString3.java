29c9c47654dfb583f39a1d1c469f2db6e4365c9c
g7tianyi <g7tianyi@gmail.com>
2019-08-26 16:19:28 +0800

    feat#lintcode#g7tianyi#reverse-words-in-a-string-iii

---
+++ src/main/java/com/g7tianyi/lintcode/string/ReverseString3.java
@@ -0,0 +1,82 @@
+package com.g7tianyi.lintcode.string;
+
+import com.g7tianyi.lintcode.util.Log;
+import lombok.AllArgsConstructor;
+import org.junit.Test;
+
+import java.util.function.Consumer;
+
+/**
+ * Created by g7tianyi on Aug 26, 2019
+ *
+ * <p>Problem link: https://www.lintcode.com/problem/reverse-words-in-a-string-iii/description
+ */
+public class ReverseString3 {
+
+  private static final Log log = new Log();
+
+  public class Solution {
+
+    public String reverseWords(String s) {
+
+      char[] chars = s.toCharArray();
+
+      int i = 0;
+      while (i < chars.length && chars[i] == ' ') {
+        i++;
+      }
+
+      int j = i + 1;
+      while (j < chars.length) {
+        while (j < chars.length && chars[j] != ' ') {
+          j++;
+        }
+        if (j <= chars.length) {
+          reverseBetween(chars, i, j - 1);
+        }
+
+        i = j;
+        while (i < chars.length && chars[i] == ' ') {
+          i++;
+        }
+        j = i +1;
+      }
+
+      return new String(chars);
+    }
+
+    private void reverseBetween(char[] chars, int i, int j) {
+      char ch;
+      while (i < j) {
+        ch = chars[i];
+        chars[i] = chars[j];
+        chars[j] = ch;
+
+        ++i;
+        --j;
+      }
+    }
+  }
+
+  @AllArgsConstructor
+  private static class Input {
+
+    private String s;
+  }
+
+  @Test
+  public void test() {
+
+    Solution s = new Solution();
+
+    Consumer<Input> c = input -> log.info("[%s] => [%s]", input.s, s.reverseWords(input.s));
+
+    c.accept(new Input("hello"));
+    c.accept(new Input("hello world"));
+    c.accept(new Input(" hello world"));
+    c.accept(new Input(" hello    world"));
+    c.accept(new Input(" hello    world"));
+    c.accept(new Input(" hello    world  "));
+    c.accept(new Input("s'teL ekat edoCteeL tsetnoc"));
+  }
+}
