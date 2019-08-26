3e78d382e7fe653e41efd817daaba481dde8592f
g7tianyi <g7tianyi@gmail.com>
2019-08-26 19:29:33 +0800

    feat#lintcode#g7tianyi#range-sum-query-immutable

---
+++ src/main/java/com/g7tianyi/lintcode/dp/RangeSumQueryImmutable.java
@@ -0,0 +1,28 @@
+package com.g7tianyi.lintcode.dp;
+
+/**
+ * Created by g7tianyi on Aug 25, 2019
+ *
+ * <p>Problem link: https://www.lintcode.com/problem/range-sum-query-immutable/description
+ */
+public class RangeSumQueryImmutable {
+
+  class NumArray {
+
+    private int[] arr;
+
+    public NumArray(int[] arr) {
+      for (int i = 1; i < arr.length; i++) {
+        arr[i] += arr[i - 1];
+      }
+      this.arr = arr;
+    }
+
+    public int sumRange(int i, int j) {
+      if (i == 0) {
+        return this.arr[j];
+      }
+      return this.arr[j] - this.arr[i];
+    }
+  }
+}
