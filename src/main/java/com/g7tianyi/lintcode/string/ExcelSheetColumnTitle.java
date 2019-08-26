6b69f0c3f6929b4bfe9a4205d993d79cc3d3d9bb
g7tianyi <g7tianyi@gmail.com>
2019-08-26 15:18:40 +0800

    feat#lintcode#g7tianyi#excel-sheet-column-title

---
+++ src/main/java/com/g7tianyi/lintcode/string/ExcelSheetColumnTitle.java
@@ -0,0 +1,74 @@
+package com.g7tianyi.lintcode.string;
+
+import com.g7tianyi.lintcode.util.Log;
+import lombok.AllArgsConstructor;
+import org.junit.Test;
+
+import java.util.function.Consumer;
+
+/**
+ * Created by g7tianyi on Aug 26, 2019
+ *
+ * <p>Problem link: https://www.lintcode.com/problem/excel-sheet-column-title/description
+ */
+public class ExcelSheetColumnTitle {
+
+  private static final Log log = new Log();
+
+  public class Solution {
+
+    public String convertToTitle(int num) {
+
+      StringBuilder sb = new StringBuilder();
+
+      long mod = 26, rem; // 要用long，神坑啊...
+      char ch;
+      while (num != 0) {
+        rem = num % mod;
+
+        if (rem == 0) {
+          rem = mod;
+          ch = 'Z';
+        } else {
+          ch = (char) (65 + rem / (mod / 26) - 1); // 65 即 'A'
+        }
+
+        sb.append(ch);
+
+        num -= rem;
+        mod *= 26;
+
+        log.info("%d %d %d", num, rem, mod / 26);
+      }
+
+      sb.reverse();
+
+      return sb.toString();
+    }
+  }
+
+  @AllArgsConstructor
+  private static class Input {
+
+    private int num;
+  }
+
+  @Test
+  public void test() {
+
+    Solution s = new Solution();
+
+    Consumer<Input> c = input -> log.info("[%d] => [%s]", input.num, s.convertToTitle(input.num));
+
+    c.accept(new Input(1));
+    c.accept(new Input(26));
+    c.accept(new Input(28));
+    c.accept(new Input(29));
+    c.accept(new Input(52));
+    c.accept(new Input(25 * 26 + 26));
+    c.accept(new Input(26 * 26 + 26));
+    c.accept(new Input(3 * 26 * 26 * 26 + 8 * 26 * 26 + 12));
+    c.accept(new Input(10086));
+    c.accept(new Input(1000000000));
+  }
+}
