24b1a8c96029a607fb64f8611e714bc9260512b5
g7tianyi <g7tianyi@gmail.com>
2019-08-27 19:11:21 +0800

    feat#lintcode#g7tianyi#flatten-list

---
+++ src/main/java/com/g7tianyi/lintcode/math/FlattenList.java
@@ -0,0 +1,54 @@
+package com.g7tianyi.lintcode.math;
+
+import com.g7tianyi.lintcode.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Created by g7tianyi on Aug 27, 2019
+ *
+ * <p>Problem link: https://www.lintcode.com/problem/flatten-list/description
+ */
+public class FlattenList {
+
+  private static final Log log = new Log();
+
+  public interface NestedInteger {
+
+    boolean isInteger();
+
+    Integer getInteger();
+
+    List<NestedInteger> getList();
+  }
+
+  public static class Solution {
+
+    public List<Integer> flatten(List<NestedInteger> nestedIntegers) {
+
+      Stack<NestedInteger> stack = new Stack<>();
+      pushToStack(nestedIntegers, stack);
+
+      List<Integer> result = new ArrayList<>();
+      while (!stack.isEmpty()) {
+        NestedInteger nestedInteger = stack.pop();
+        if (nestedInteger.isInteger()) {
+          result.add(nestedInteger.getInteger());
+        } else {
+          pushToStack(nestedInteger.getList(), stack);
+        }
+      }
+
+      return result;
+    }
+
+    public static void pushToStack(List<NestedInteger> nestedIntegers, Stack<NestedInteger> stack) {
+      for (int i = nestedIntegers.size() - 1; i >= 0; --i) {
+        NestedInteger nestedInteger = nestedIntegers.get(i);
+        stack.push(nestedInteger);
+      }
+    }
+  }
+}
