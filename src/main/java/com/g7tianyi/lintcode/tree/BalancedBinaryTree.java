a2ee4518e78adc60c3777fe5ff259eec8c82afb4
g7tianyi <g7tianyi@gmail.com>
2019-08-27 21:03:15 +0800

    feat#lintcode#g7tianyi#balanced-binary-tree

---
+++ src/main/java/com/g7tianyi/lintcode/tree/BalancedBinaryTree.java
@@ -0,0 +1,83 @@
+package com.g7tianyi.lintcode.tree;
+
+import com.g7tianyi.lintcode.common.TreeNode;
+import com.g7tianyi.lintcode.util.Log;
+import lombok.AllArgsConstructor;
+import org.junit.Test;
+
+import java.util.function.Consumer;
+
+/**
+ * Created by g7tianyi on Aug 27, 2019
+ *
+ * <p>Problem link: https://www.lintcode.com/problem/balanced-binary-tree/description
+ */
+public class BalancedBinaryTree {
+
+  private static final Log log = new Log();
+
+  public class Solution {
+
+    private class NodeInfo {
+
+      private boolean balanced;
+
+      private int depth;
+
+      public NodeInfo(boolean balanced, int depth) {
+        this.balanced = balanced;
+        this.depth = depth;
+      }
+    }
+
+    public boolean isBalanced(TreeNode root) {
+      if (root == null) {
+        return true;
+      }
+
+      NodeInfo nodeInfo = traverse(root);
+
+      return nodeInfo.balanced;
+    }
+
+    private NodeInfo traverse(TreeNode root) {
+      boolean leftBalanced = true;
+      int leftDepth = 0;
+      if (root.left != null) {
+        NodeInfo leftInfo = traverse(root.left);
+        leftBalanced = leftInfo.balanced;
+        leftDepth = leftInfo.depth;
+      }
+
+      boolean rightBalanced = true;
+      int rightDepth = 0;
+      if (root.right != null) {
+        NodeInfo rightInfo = traverse(root.right);
+        rightBalanced = rightInfo.balanced;
+        rightDepth = rightInfo.depth;
+      }
+
+      boolean balanced = leftBalanced && rightBalanced && (Math.abs(rightDepth - leftDepth) < 2);
+      int depth = Math.max(leftDepth, rightDepth) + 1;
+      return new NodeInfo(balanced, depth);
+    }
+  }
+
+  @AllArgsConstructor
+  private static final class Input {
+
+    private TreeNode root;
+  }
+
+  @Test
+  public void test() {
+
+    Solution s = new Solution();
+
+    Consumer<Input> runner = input -> log.info("%s", s.isBalanced(input.root));
+
+    runner.accept(new Input(TreeNode.createTree("1,2,3")));
+    runner.accept(new Input(TreeNode.createTree("3,9,20,#,#,15,7")));
+    runner.accept(new Input(TreeNode.createTree("1,#,2,#,#,3,4")));
+  }
+}
