7c1b95cdac809a8ac2c930680266fa7e7008e954
g7tianyi <g7tianyi@gmail.com>
2019-08-31 21:59:22 +0800

    feat#g7tianyi#PowerOfTwo

---
+++ src/main/java/com/g7tianyi/lintcode/math/PowerOfTwo.java
@@ -0,0 +1,16 @@
+package com.g7tianyi.lintcode.math;
+
+/**
+ * Created by g7tianyi on Aug 31, 2019
+ *
+ * @link https://www.lintcode.com/problem/power-of-three/note/192914
+ */
+public class PowerOfTwo {
+
+  public class Solution {
+
+    public boolean isPowerOfTwo(int num) {
+      return num > 0 && (num & (num - 1)) == 0;
+    }
+  }
+}
