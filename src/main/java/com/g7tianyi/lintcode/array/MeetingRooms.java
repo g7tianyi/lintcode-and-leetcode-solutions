7247ce7620c4803018c6f40d025088be1129a055
g7tianyi <g7tianyi@gmail.com>
2019-08-31 23:11:48 +0800

    feat#g7tianyi#MeetingRooms

---
+++ src/main/java/com/g7tianyi/lintcode/array/MeetingRooms.java
@@ -0,0 +1,85 @@
+package com.g7tianyi.lintcode.array;
+
+import com.g7tianyi.util.Logger;
+import com.google.common.collect.Lists;
+import lombok.AllArgsConstructor;
+import org.junit.Test;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * Created by g7tianyi on Aug 31, 2019
+ *
+ * @link https://www.lintcode.com/problem/meeting-rooms/description
+ */
+public class MeetingRooms {
+
+  private static final Logger log = Logger.getInstance();
+
+  public class Interval {
+
+    int start, end;
+
+    Interval(int start, int end) {
+      this.start = start;
+      this.end = end;
+    }
+
+    @Override
+    public String toString() {
+      return String.format("[%d, %d]", start, end);
+    }
+  }
+
+  public class Solution {
+
+    public boolean canAttendMeetings(List<Interval> intervals) {
+
+      if (intervals == null || intervals.isEmpty()) {
+        return false;
+      }
+
+      intervals.sort(
+          new Comparator<Interval>() {
+            @Override
+            public int compare(Interval o1, Interval o2) {
+              if (o1.start != o2.start) {
+                return Integer.compare(o1.start, o2.start);
+              }
+              return Integer.compare(o1.end, o2.end);
+            }
+          });
+
+      int prevEnd = intervals.get(0).end;
+      for (int i = 1; i < intervals.size(); ++i) {
+        Interval interval = intervals.get(i);
+        if (interval.start < prevEnd) {
+          return false;
+        }
+      }
+
+      return true;
+    }
+  }
+
+  @AllArgsConstructor
+  private static class Case {
+
+    private List<Interval> intervals;
+  }
+
+  @Test
+  public void test() {
+
+    Solution s = new Solution();
+
+    Consumer<Case> c = aCase -> log.info(s.canAttendMeetings(aCase.intervals));
+
+    c.accept(
+        new Case(
+            Lists.newArrayList(new Interval(0, 30), new Interval(5, 10), new Interval(15, 20))));
+    c.accept(new Case(Lists.newArrayList(new Interval(5, 8), new Interval(9, 15))));
+  }
+}
