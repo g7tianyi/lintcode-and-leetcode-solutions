fbe719b7c4e9c496ee1bfe71010f4b6618de76c4
g7tianyi <g7tianyi@gmail.com>
2019-09-01 00:47:24 +0800

    feat#g7tianyi#BinaryTreeMaximumNode

---
+++ src/main/java/com/g7tianyi/lintcode/tree/BinaryTreeMaximumNode.java
@@ -0,0 +1,39 @@
+package com.g7tianyi.lintcode.tree;
+
+import com.g7tianyi.common.TreeNode;
+
+/**
+ * Created by g7tianyi on Sep 1, 2019
+ *
+ * @link https://www.lintcode.com/problem/binary-tree-maximum-node/description
+ */
+public class BinaryTreeMaximumNode {
+
+  public class Solution {
+
+    public TreeNode maxNode(TreeNode root) {
+
+      if (root == null) {
+        return null;
+      }
+
+      TreeNode maxLeft = null;
+      if (root.left != null) {
+        maxLeft = maxNode(root.left);
+      }
+      TreeNode maxRight = null;
+      if (root.right != null) {
+        maxRight = maxNode(root.right);
+      }
+
+      TreeNode result = root;
+      if (maxLeft != null && maxLeft.val > result.val) {
+        result = maxLeft;
+      }
+      if (maxRight != null && maxRight.val > result.val) {
+        result = maxRight;
+      }
+      return result;
+    }
+  }
+}
