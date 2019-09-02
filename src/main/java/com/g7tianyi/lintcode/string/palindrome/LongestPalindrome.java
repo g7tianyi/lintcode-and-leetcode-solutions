35be675b11fc7f4725da646b3bb026ff62c0e1a0
g7tianyi <g7tianyi@gmail.com>
2019-09-02 13:38:17 +0800

    feat#g7tianyi#LongestPalindrome. Palindrome summary.

---
+++ src/main/java/com/g7tianyi/lintcode/string/palindrome/LongestPalindrome.java
@@ -0,0 +1,37 @@
+import com.g7tianyi.util.Logger;
+import lombok.AllArgsConstructor;
+import org.junit.Test;
+
+import java.util.function.Consumer;
+
+/**
+ * Created by g7tianyi on Sep 02, 2019
+ *
+ * @link 
+ */
+public class LongestPalindrome {
+
+  private static final Logger log = Logger.getInstance();
+
+  public class Solution {
+  
+  
+  
+  }
+  
+  @AllArgsConstructor
+  private class Case {
+
+  }
+  
+   @Test
+  public void test() {
+
+    Solution s = new Solution();
+
+    Consumer<Case> c = aCase -> {
+    
+    };
+
+  }
+}
