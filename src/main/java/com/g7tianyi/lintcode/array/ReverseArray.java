8e34fe8c9a8e772732c33ae6948d3ac6e5ccd9c1
g7tianyi <g7tianyi@gmail.com>
2019-09-03 17:18:36 +0800

    feat#g7tianyi#Boring - ReverseArray

---
+++ src/main/java/com/g7tianyi/lintcode/array/ReverseArray.java
@@ -0,0 +1,33 @@
+package com.g7tianyi.lintcode.array;
+
+import com.g7tianyi.util.Logger;
+
+/**
+ * Created by g7tianyi on Sep 03, 2019
+ *
+ * @link https://www.lintcode.com/problem/reverse-array/description
+ */
+public class ReverseArray {
+
+  private static final Logger log = Logger.getInstance();
+
+  public class Solution {
+
+    public void reverseArray(int[] nums) {
+
+      if (nums == null || nums.length < 2) {
+        return;
+      }
+
+      int i = 0, j = nums.length - 1, temp;
+      while (i < j) {
+        temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+
+        ++i;
+        --j;
+      }
+    }
+  }
+}
