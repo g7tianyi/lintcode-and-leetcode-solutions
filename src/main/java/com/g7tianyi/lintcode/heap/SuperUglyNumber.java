38708cce684211bb0d854562af04642b0c9cb31e
g7tianyi <g7tianyi@gmail.com>
2019-10-24 09:58:33 +0800

    feat#g7tianyi#Stack - FlattenNestedListIterator

--- src/main/java/com/g7tianyi/lintcode/heap/SuperUglyNumber.java
+++ src/main/java/com/g7tianyi/lintcode/heap/SuperUglyNumber.java
@@ -1,4 +1,4 @@
-package com.g7tianyi.lintcode.permutation;
+package com.g7tianyi.lintcode.heap;
 
 import com.g7tianyi.util.Logger;
 import org.junit.Test;
--- src/main/java/com/g7tianyi/lintcode/stack/FlattenNestedListIterator.java
+++ src/main/java/com/g7tianyi/lintcode/stack/FlattenNestedListIterator.java
@@ -1,26 +1,54 @@
-import com.g7tianyi.util.Logger;
-import org.junit.Test;
+package com.g7tianyi.lintcode.stack;
 
+import com.g7tianyi.lintcode.list.NestedInteger;
+import com.g7tianyi.util.Logger;
+
+import java.util.Iterator;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * Created by g7tianyi on Oct 24, 2019
  *
- * @link
+ * @link https://www.lintcode.com/problem/flatten-nested-list-iterator/description
  */
 public class FlattenNestedListIterator {
 
   private static final Logger log = Logger.getInstance();
 
-  public class Solution {
+  public class NestedIterator implements Iterator<Integer> {
 
-  }
+    Stack<NestedInteger> stack = new Stack<>();
 
-  private final Solution s = new Solution();
+    public NestedIterator(List<NestedInteger> nestedList) {
+      if (nestedList == null || nestedList.size() == 0) {
+        return;
+      }
 
-  @Test
-  public void test() {
+      for (int i = nestedList.size() - 1; i >= 0; i--) {
+        stack.push(nestedList.get(i));
+      }
+    }
 
+    @Override
+    public Integer next() {
+      return hasNext() ? stack.pop().getInteger() : null;
+    }
 
+    @Override
+    public boolean hasNext() {
+      while (!stack.isEmpty()) {
+        NestedInteger nestedInteger = stack.peek();
+        if (nestedInteger.isInteger()) {
+          return true;
+        }
+
+        stack.pop();
+        for (int i = nestedInteger.getList().size() - 1; i >= 0; i--) {
+          stack.push(nestedInteger.getList().get(i));
+        }
+      }
+      return false;
+    }
   }
 }
